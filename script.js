1c96e6270c1a8488a12c31cdb2f6673cc183c83f
Amrutha Patil <ap7982@nyu.edu>
2024-03-09 16:24:37 -0500

    update layer 2

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
+// Add smooth scrolling to all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
